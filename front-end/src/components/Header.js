65c912c02627854f76a2b33dfb2062569c62da14
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2024-11-06 21:11:35 -0500

    first commit

---
+++ front-end/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+import Navigation from './Navigation'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <Navigation/>
+    </div>
+  )
+}
+
+export default Header
